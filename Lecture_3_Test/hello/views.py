51122f69937f970126f90e87622119e8bd5023e7
Jessica Mansell <jcmansell88@gmail.com>
2022-06-28 11:08:08 -0500

    adding proj 1 files + lecture 3

--- Lecture_3_Test/hello/views.py
+++ Lecture_3_Test/hello/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello!")
+
+def jessica(request):
+    return HttpResponse("Hello, Jessica!")
