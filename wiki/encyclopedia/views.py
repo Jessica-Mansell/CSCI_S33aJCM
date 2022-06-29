0be07baba334b6b47d10c99de23a73c1e8fa7b96
Jessica Mansell <jcmansell88@gmail.com>
2022-06-29 10:32:04 -0500

    adding css.html paths

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -6,5 +6,10 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    }),
+
+def css(request):
+    return render(request, "encyclopedia/css.html", {
+        "entries": util.list_entries()
     })
 
