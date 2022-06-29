3ff213846e4e5f878169189988abea3786ff0c25
Jessica Mansell <jcmansell88@gmail.com>
2022-06-28 21:45:29 -0500

    messing around

--- Lecture_3_Test/hello/views.py
+++ Lecture_3_Test/hello/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello!")
 
-def jessica(request):
-    return HttpResponse("Hello, Jessica!")
+def index(request):
+    return HttpResponse("Hello, world!")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
