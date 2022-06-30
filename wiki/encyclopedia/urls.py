04d0e76c335d66b35d3ab33418f1a7a433d1f15d
Jessica Mansell <jcmansell88@gmail.com>
2022-06-30 00:55:30 -0500

    lamesauce

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 
 from . import views
 
-app_name ="wiki"
-
 urlpatterns = [
     path('', views.index, name="index"),
-    path('', views.info_page, name="title")
+    path('entries/<str:title>', views.info_page, name="title")
 ]
