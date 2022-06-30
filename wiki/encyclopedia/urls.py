69c1273a22d144372771a77ce7312359d2129581
Jessica Mansell <jcmansell88@gmail.com>
2022-06-30 09:29:00 -0500

    yikes again

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib import admin
-
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('entries/<str:title>', views.info_page, name="title")
+    path("wiki/<str:title>", views.link_page, name="entrypage"),
 ]
